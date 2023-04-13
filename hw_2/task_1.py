084db5f4af5e411ba6d2c3e549b0172454c3931e
oleg086rus <o.pototskiy@yandex.ru>
2023-04-14 03:01:02 +0500

    hw2

--- hw_2/task_1.py
+++ hw_2/task_1.py
@@ -3,17 +3,21 @@
 # перевернуть, чтобы все монетки были повернуты вверх одной и той же
 # стороной. Выведите минимальное количество монет, которые нужно
 # перевернуть
-
-quantity = int(input())
-count_tails = 0
-count_eagle = 0
-for i in range(quantity):
-    x = int(input())
-    if x == 0:
-        count_tails += 1
+def coin():
+    quantity = int(input('Введите количество монет на столе: '))
+    count_tails = 0
+    count_eagle = 0
+    for i in range(quantity):
+        print('Введите 0 или 1. 0 - решка, 1 - орел.')
+        x = int(input())
+        if x == 0:
+            count_tails += 1
+        else:
+            count_eagle += 1
+    print('Необходимо сделать действий: ')
+    if count_eagle > count_tails:
+        print(count_tails)
     else:
-        count_eagle += 1
-if count_eagle > count_tails:
-    print(count_tails)
-else:
-    print(count_eagle)
+        print(count_eagle)
+
+coin()
--- hw_2/task_2.py
+++ hw_2/task_2.py
@@ -3,10 +3,13 @@
 # (X,Y≤1000), а Катя должна их отгадать. Для этого Петя делает две подсказки.
 # Он называет сумму этих чисел S и их произведение P. Помогите Кате отгадать
 # задуманные Петей числа
-
-x = int(input())
-y = int(input())
-for i in range(x):
-    for j in range(y):
-        if x == i + j and y == i * j:
-            print(i, j)
+def math():
+    x = int(input('Введите сумму чисел: '))
+    y = int(input('Введите произведение чисел: '))
+    z = 1000
+    print('Числа: ')
+    for i in range(z):
+        for j in range(z):
+            if x == i + j and y == i * j:
+                print(i, j)
+math()
